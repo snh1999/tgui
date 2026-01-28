0ec3d1485b14b7598ccb9b42735a326efbbb4440
sahriar nur <shariarnurnahin99@gmail.com>
2026-01-28 19:21:04 +0600

    feat(tgui-19): adds icon packages and updates config

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,47 @@
 /** biome-ignore-all lint/complexity/useSimplifiedLogicExpression: <not necessary for setup file> */
 
+import {fileURLToPath, URL} from 'node:url' // Use node: protocol
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
-import { defineConfig } from "vite";
+import Icons from 'unplugin-icons/vite'
 
-// @ts-expect-error process is a nodejs global
+import {defineConfig} from "vite";
+
+
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [vue(), tailwindcss()],
-
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent Vite from obscuring rust errors
-  clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
-  server: {
-    port: 1420,
-    strictPort: true,
-    host: host || false,
-    hmr: host
-      ? {
-        protocol: "ws",
-        host,
-        port: 1421,
-      }
-      : undefined,
-    watch: {
-      // 3. tell Vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
+    plugins: [vue(), tailwindcss(),
+        Icons({
+            autoInstall: true, // Auto-install icon sets when used
+            compiler: 'vue3', // or 'vue2' for Vue 2
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     },
-  },
+    // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+    //
+    // 1. prevent Vite from obscuring rust errors
+    clearScreen: false,
+    // 2. tauri expects a fixed port, fail if that port is not available
+    server: {
+        port: 1420,
+        strictPort: true,
+        host: host || false,
+        hmr: host
+            ? {
+                protocol: "ws",
+                host,
+                port: 1421,
+            }
+            : undefined,
+        watch: {
+            // 3. tell Vite to ignore watching `src-tauri`
+            ignored: ["**/src-tauri/**"],
+        },
+    },
 }));
