4f1ef1f920f827a17342da23ce2b29a309b18cdd
sahriar nur <shariarnurnahin99@gmail.com>
2026-02-04 09:33:41 +0600

    fix: biome formatting issue

--- src/components/ui/sidebar/SidebarInput.vue
+++ src/components/ui/sidebar/SidebarInput.vue
@@ -10,13 +10,11 @@
 
 <template>
   <Input
-      data-slot="sidebar-input"
-      data-sidebar="input"
-      :class="cn(
+    data-slot="sidebar-input"
+    data-sidebar="input"
+    :class="cn(
       'bg-background h-8 w-full shadow-none',
       props.class,
     )"
-  >
-    <slot/>
-  </Input>
+  />
 </template>
