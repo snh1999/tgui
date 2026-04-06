f312b6140d7765669c3d5a756e097676c9ff0120
sahriar nur <shariarnurnahin99@gmail.com>
2026-04-05 23:49:35 -0600

    code and type cleanup

--- src/app.constants.ts
+++ src/app.constants.ts
@@ -1,3 +1,7 @@
 export const CATEGORY_FORM_ID = "category-form";
 export const GROUP_FORM_ID = "group-form";
 export const COMMAND_FORM_ID = "command-form";
+
+export const LOG_PERSISTENCE_VERSION = 1;
+export const LOG_KEY_PREFIX = "exec_logs:";
+export const MAX_PERSISTED_LOGS = 5000;
